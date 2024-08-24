a75cfdb4ecf568038f35f36fccbb43207f1a4067
Mmarcelinho <marcelorosario2001@gmail.com>
2024-08-24 19:21:57 -0300

    feat(Result): adiciona a interface IResult e a classe Result para padronização de resultados

---
+++ src/Backend/SistemaCadastro.Application/Shared/Results/Result.cs
@@ -0,0 +1,29 @@
+using SistemaCadastro.Application.Shared.Results.Interfaces;
+using SistemaCadastro.Domain.Notifications;
+
+namespace SistemaCadastro.Application.Shared.Results;
+
+public class Result : IResult
+{
+    private List<Notification> _notifications;
+
+    public Result(int resultCode, string message, bool isOk)
+    {
+        ResultCode = resultCode;
+        Message = message;
+        IsOk = isOk;
+        _notifications = [];
+    }
+
+    public int ResultCode { get; private set; }
+    public string Message { get; private set; }
+    public bool IsOk { get; private set; }
+    public object Data { get; private set; }
+    public IReadOnlyCollection<Notification> Notifications => _notifications;
+
+    public void SetNotifications(List<Notification> notifications)
+    => _notifications = notifications;
+
+    public void SetData(object data) => Data = data;
+}
+
