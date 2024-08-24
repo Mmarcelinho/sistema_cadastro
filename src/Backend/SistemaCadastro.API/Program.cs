cd8ead898e4632ba400bfdfc25fddb6534dc475d
Mmarcelinho <marcelorosario2001@gmail.com>
2024-08-24 20:26:15 -0300

    feat(API): adiciona projeto SistemaCadastro.API com configuração inicial feat(API): implementa configuração de logging e endpoints no Program.cs feat(API): cria arquivos de configuração para desenvolvimento e produção chore(sln): atualiza a solução para incluir o projeto SistemaCadastro.API

---
+++ src/Backend/SistemaCadastro.API/Program.cs
@@ -0,0 +1,21 @@
+using SistemaCadastro.Application;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
