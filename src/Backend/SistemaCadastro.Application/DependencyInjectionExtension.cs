ed215cd9c62882c1434ef18a39bb9485b53b6545
Mmarcelinho <marcelorosario2001@gmail.com>
2024-08-26 00:13:16 -0300

    feat(CadastroController): adiciona rotas específicas para pessoa física e jurídica feat(CadastroController): altera resposta para Created em vez de Ok feat(Program): habilita URLs em minúsculas e configura Swagger chore(.csproj): adiciona referência ao EntityFrameworkCore.Design e ao projeto de infraestrutura chore(DependencyInjectionExtension): inclui namespaces necessários para resultados compartilhados

--- src/Backend/SistemaCadastro.Application/DependencyInjectionExtension.cs
+++ src/Backend/SistemaCadastro.Application/DependencyInjectionExtension.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using SistemaCadastro.Application.Shared.Results;
+using SistemaCadastro.Application.Shared.Results.Interfaces;
 
 namespace SistemaCadastro.Application;
 
